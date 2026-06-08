be2dd10e79e1ed2369a61864b5be1915254188dd
Manoj Y G <manojgangadhar20@gmail.com>
2026-06-08 14:15:15 +0530

    model schema is added to codebase

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,7 +1,62 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const Blog = mongoose.model("Blog", blogSchema);
-// ready to go!
+const userSchema = new Schema({
+  avatar: {
+    type: {
+      url: String,
+      localPath: String,
+    },
+    default: {
+      url: `https://placehold.co/200x200`,
+      localPath: "",
+    },
+  },
+  username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true,
+  },
+  email:{
+    type:String,
+    required:true,
+    unique:true,
+    lowercase:true,
+    trim:true,
+  },
+  fullName:{
+    type:String,
+    trim:true,
+  },
+  password:{
+    type:String,
+    required:[true,"Password is Required"]
+  },
+  isEmailVerified:{
+    type:Boolean,
+    default:false,
+  },
+  refreshToken:{
+    type:String
+  },
+  forgetPasswordToken:{
+    type:String
+  },
+  forgetPasswordExpiry:{
+    type:Date
+  },
+  emailVerificationToken:{
+    type:String
+  },
+  emailVerificationExpiry:{
+    type:Date
+  }
 
-const schema = new Schema();
-schema.path('_id');
+},{
+    timestamps:true,
+},
+);
+
+export const User = mongoose.model("User", userSchema);
