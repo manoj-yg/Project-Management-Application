f0aa94d8b9494c82db4dd4fb545c8dc35c0fd3e1
Manoj Y G <manojgangadhar20@gmail.com>
2026-06-12 12:08:20 +0530

    User model is updated

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-
+import bcrypt from "bcrypt";
 const userSchema = new Schema({
   avatar: {
     type: {
@@ -59,4 +59,16 @@ const userSchema = new Schema({
 },
 );
 
+userSchema.pre("save",async function(next){
+  if(!this.isModified("password")) return next()
+
+  this.password=await bcrypt.hash(this.password,10)
+  next()
+});
+
+userSchema.methods.isPasswordCorrect=async function(password){
+  return await bcrypt.compare(password,this.password);
+};
+
+// JWT has three parts header,payload,signature(xxx.yyyy.zzzz)
 export const User = mongoose.model("User", userSchema);
